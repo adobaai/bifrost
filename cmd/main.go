0be364a41847234e6136f06213bc76ceb5c21f99
ionling <soarvision@outlook.com>
2023-03-30 16:06:55 +0800

    feat: add cmd

---
+++ cmd/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+
+	"github.com/go-logr/stdr"
+
+	"github.com/adobaai/bifrost/internal/config"
+	"github.com/adobaai/bifrost/internal/server"
+)
+
+func main() {
+	if err := run(); err != nil {
+		fmt.Println("ERR:", err)
+	}
+}
+
+func run() error {
+	l := stdr.New(log.Default())
+	conf, err := config.LoadFile("config.json")
+	if err != nil {
+		return fmt.Errorf("load config: %w", err)
+	}
+
+	stdr.SetVerbosity(conf.Log.Level)
+
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
+
+	server := server.New(conf, l)
+	go func() {
+		<-c
+		if err := server.Stop(); err != nil {
+			l.Error(err, "stop server")
+		}
+	}()
+	return server.Start()
+}
---
+++ go.mod
@@ -0,0 +1,25 @@
+module github.com/adobaai/bifrost
+
+go 1.19
+
+require (
+	github.com/go-logr/logr v1.2.3
+	github.com/go-logr/stdr v1.2.2
+	github.com/nknorg/encrypted-stream v1.0.1
+	github.com/sourcegraph/conc v0.2.0
+	go.uber.org/multierr v1.10.0
+	golang.org/x/net v0.8.0
+)
+
+/// Test
+require github.com/stretchr/testify v1.8.2
+
+require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/imdario/mergo v0.3.15 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	golang.org/x/crypto v0.7.0 // indirect
+	golang.org/x/sys v0.6.0 // indirect
+	golang.org/x/text v0.8.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
